44b8965b9cfa7b231a13a4016c86602c172ba14f
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-01-06 23:21:11 +0530

    git commit -m"addition"

---
+++ Calculator/Addition.java
@@ -0,0 +1,12 @@
+package Calculator;
+import java.util.Scanner;
+
+public class Addition {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int x = scan.nextInt();
+    int y =scan.nextInt();
+
+    System.out.println(x+y);
+  }
+}
